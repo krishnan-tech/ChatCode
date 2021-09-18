03519146acdb8a81b6815f7e8820c50bf25cade7
krishnan-tech <krishnannavadia@gmail.com>
2021-09-18 20:49:55 +0530

    chat section UI

--- client/src/components/ChatDiscord.tsx
+++ client/src/components/ChatDiscord.tsx
@@ -1,50 +1,90 @@
 import { ChatIcon } from "@chakra-ui/icons";
-import {
-  Box,
-  Button,
-  Drawer,
-  DrawerBody,
-  DrawerCloseButton,
-  DrawerContent,
-  DrawerFooter,
-  DrawerHeader,
-  Input,
-  useDisclosure,
-} from "@chakra-ui/react";
-
+import { Box, Button, Flex, Input, useDisclosure } from "@chakra-ui/react";
+import { format } from "path";
 import React, { useState } from "react";
 
-import { Center, Square, Circle } from "@chakra-ui/react"
-
 export const ChatDiscordButton = () => {
-    const { isOpen, onOpen, onClose } = useDisclosure();
-    const btnRef = React.useRef();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = React.useRef();
 
-    return (
-      <>
-        <Button ref={btnRef} colorScheme="teal" onClick={onOpen}>
-          {/* Open */}
-          <ChatIcon />
-        </Button>
-
-      </>
-    );
-  };
-
+  return (
+    <>
+      <Button ref={btnRef} colorScheme="teal" onClick={onOpen}>
+        {/* Open */}
+        <ChatIcon />
+      </Button>
+    </>
+  );
+};
 
 export const ChatDiscord = () => {
-    const { isOpen, onOpen, onClose } = useDisclosure();
-    const btnRef = React.useRef();
+  const btnRef = React.useRef();
+  const [inputState, setInputState] = useState("");
 
-    return (
-      <>
-
-      <Box>
-
-    Chat
+  return (
+    <Box>
+      <Box
+        bg="facebook.200"
+        w="100%"
+        p={4}
+        color="white"
+        rounded="base"
+        mb="4"
+        // key={msg._id}
+      >
+        <Flex>
+          <Box color="facebook.800">Krishnan</Box>
+          <Box color="facebook.800" ml={4}>
+            2 hour ago
+          </Box>
+        </Flex>
+        <Box color="blackAlpha.800">Hello there</Box>
+      </Box>
+      <Box
+        bg="facebook.200"
+        w="100%"
+        p={4}
+        color="white"
+        rounded="base"
+        mb="4"
+        // key={msg._id}
+      >
+        <Flex>
+          <Box color="facebook.800">Krishnan</Box>
+          <Box color="facebook.800" ml={4}>
+            2 hour ago
+          </Box>
+        </Flex>
+        <Box color="blackAlpha.800">Hello there</Box>
+      </Box>
+      <Box
+        bg="facebook.200"
+        w="100%"
+        p={4}
+        color="white"
+        rounded="base"
+        mb="4"
+        // key={msg._id}
+      >
+        <Flex>
+          <Box color="facebook.800">Krishnan</Box>
+          <Box color="facebook.800" ml={4}>
+            2 hour ago
+          </Box>
+        </Flex>
+        <Box color="blackAlpha.800">Hello there</Box>
       </Box>
 
-
-      </>
-    );
-  };
+      <Flex position="fixed" bottom="10">
+        <Input
+          placeholder="Type here..."
+          value={inputState}
+          onChange={(e) => setInputState(e.target.value)}
+        />
+        <Button colorScheme="blue" ml="2">
+          send
+        </Button>
+      </Flex>
+    </Box>
+  );
+};
