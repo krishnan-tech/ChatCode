385d05dd9fd0f8b27d27ca746d87a11db9bf7e59
krishnan-tech <krishnannavadia@gmail.com>
2021-09-19 13:48:44 +0530

    audio socket

--- socket/src/index.ts
+++ socket/src/index.ts
@@ -6,7 +6,13 @@ const io = require("socket.io")(8900, {
 
 console.log("socket started has started");
 
+var socketsStatus = {};
+
 io.on("connection", (socket: any) => {
+  const socketId = socket.id;
+  // @ts-ignore
+  socketsStatus[socket.id] = {};
+
   // listen to chatMessage
   socket.on("chatMessage", (msg: string) => {
     io.emit("message", msg);
@@ -15,5 +21,18 @@ io.on("connection", (socket: any) => {
   // listen for event inn editor
   socket.on("editor", (val: string) => {
     io.emit("editor", val);
+  });
+
+  socket.on("voice", function (data: string) {
+    var newData = data.split(";");
+    newData[0] = "data:audio/ogg;";
+    // @ts-ignore
+    newData = newData[0] + newData[1];
+
+    for (const id in socketsStatus) {
+      // @ts-ignore
+      if (id != socketId && !socketsStatus[id].mute && socketsStatus[id].online)
+        socket.broadcast.to(id).emit("send", newData);
+    }
   });
 });
