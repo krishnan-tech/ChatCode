48b77572f61d6567305c2dd707789346f67572ee
krishnan-tech <krishnannavadia@gmail.com>
2021-10-03 12:48:47 +0530

    Solve cors problem in axios

--- client/src/utils/axiosInstance.tsx
+++ client/src/utils/axiosInstance.tsx
@@ -1,7 +1,12 @@
-import axios from 'axios';
+import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://api.paiza.io:80/runners"
+  baseURL: "http://api.paiza.io:80/runners",
+  withCredentials: false,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+  },
 });
 
 export default API;
